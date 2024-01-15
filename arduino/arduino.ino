69d6bc212b4175cec0ccca44b4d8ebdf93e7a38f
dwunger <106967706+dwunger@users.noreply.github.com>
2024-01-15 17:25:25 -0500

    added some code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,11 +1,43 @@
+#include <ArduinoBLE.h>
+
+#define HID_KEYBOARD 0xC103
+#define DEVICE_ID "Bluetooth Keyboard Relay"
+void blePeripheralConnectHandler(BLEDevice central) {
+  // central connected event handler
+  Serial.print("Connected event, central: ");
+  Serial.println(central.address());
+}
+
+void blePeripheralDisconnectHandler(BLEDevice central) {
+  // central disconnected event handler
+  Serial.print("Disconnected event, central: ");
+  Serial.println(central.address());
+}
 
 void setup() {
-  // begin initialization
-  if (!BLE.begin()) {
-    Serial.println("starting Bluetooth® Low Energy module failed!");
+    
+    // begin initialization
+    if (!BLE.begin()) {
+        Serial.println("starting Bluetooth® Low Energy module failed!");
+    }
+    // assign event handlers for connected, disconnected to peripheral
+    BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+    BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);    
 
-    while (1);
-  }
+    BLE.setAppearance(HID_KEYBOARD);
+
+    BLE.setLocalName(DEVICE_ID);
+
+    while(BLE.connected()) {
+        Serial.print("Doing Bluetooth stuff...");
+    }
+    
+    BLE.advertise();
+
+    if (BLE.connected()) {
+        
+        BLE.disconnect();
+    }
 }
 
 void loop() {
